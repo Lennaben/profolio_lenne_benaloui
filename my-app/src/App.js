5f0f7ff5fc50161fe964aadb5ffd43c2b38dd5ae
Lennaben <BENALOUILENNA@GMAIL.COM>
2023-04-30 19:37:15 +0200

    Modif infos projet contact

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import React, { useState } from "react"
+
+function Modal() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <>
+      <button onClick={toggleModal} className='btn-ouverture'>Ouvrir la fenêtre modale</button>
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Titre de la fenêtre modale</h2>
+            <p>Contenu de la fenêtre modale</p>
+            <button onClick={toggleModal}>Fermer la fenêtre modale</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
-export default App;
+export default Modal
